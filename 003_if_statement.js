864067831cddb335c9cf8e5a218d0ca747755492
Igor Alves da Silva <Iguim.as98@gmail.com>
2022-12-26 17:00:05 -0300

    first commit

---
+++ 003_if_statement.js
@@ -0,0 +1,10 @@
+function idadex(){
+    var idade = 12;
+    if (idade >=18) {
+        console.log("maior de idade");
+    }else{
+        console.log("menor de idade");
+    }
+}
+
+console.log(idadex());
